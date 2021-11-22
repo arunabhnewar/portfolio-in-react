781fcd59fd165f0ab4959e83156b82b2fba9536f
arunabhnewar <newardevil@gmail.com>
2021-11-22 20:23:33 +0600

    about page added

---
+++ src/Components/ImageSection.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import about from '../images/about3.JPG';
+
+const ImageSection = () => {
+    return (
+        <div className="image-section">
+            <div className="img">
+                <img src={about} alt="" />
+            </div>
+
+            <div className="about-info">
+                <h4>I am<span style={{ color: 'orange' }}> Arunabh Newar</span></h4>
+                <p className="about-text">
+                    I love creating something new. I’m the happiest when I’m writing or creating something new that never exist. Web development is my passion now. That's why I learned as much as possible to build myself as a web developer. Still learning and I will continue my learning process.
+                </p>
+
+                <div className="about-details">
+                    <div className="left-section">
+                        <p>Full Name</p>
+                        <p>Age</p>
+                        <p>Nationality</p>
+                        <p>Languages</p>
+                        <p>Address</p>
+                        <p>Country</p>
+                    </div>
+                    <div className="right-section">
+                        <p>: Arunabh Newar</p>
+                        <p>: </p>
+                        <p>: Bangladeshi</p>
+                        <p>: Bangla, English, Hindi</p>
+                        <p>: Majher basti, Tabalchari, Rangamati</p>
+                        <p>: Bangladesh</p>
+                    </div>
+                </div>
+
+                <button className="btn">Download Cv</button>
+            </div>
+        </div>
+    );
+};
+
+export default ImageSection;
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import ImageSection from '../../Components/ImageSection';
+import Title from '../../Components/Title';
+import SkillSection from '../../Components/SkillSection';
+
 
 const About = () => {
     return (
         <div className="about-section">
-            <h1 style={{ color: 'white' }}>About</h1>
-        </div>
+            <Title title={'About Me'} span={'About Me'} />
+            <ImageSection />
+            <Title title={'My Skills'} span={'My Skills'} />
+
+            <div className="skillsContainer">
+                <SkillSection skill={'Javascript'} progress={'75%'} width={'75%'} />
+                <SkillSection skill={'Typescript'} progress={'15%'} width={'15%'} />
+                <SkillSection skill={'React Js'} progress={'70%'} width={'70%'} />
+                <SkillSection skill={'Express Js'} progress={'70%'} width={'70%'} />
+                <SkillSection skill={'Web Design'} progress={'85%'} width={'85%'} />
+                <SkillSection skill={'Next Js'} progress={'15%'} width={'15%'} />
+            </div>
+
+        </div >
     );
 };
 
